ea79adf2a1150ba962c19d409c2efbd7028719b4
Emmanuel Séguin IGNFAB <seguin.manu@gmail.com>
2018-06-13 21:15:15 +0200

    fix sample run

--- sample-run.sh
+++ sample-run.sh
@@ -3,7 +3,7 @@
 if [ ! -d "addok-data" ]; then
 	mkdir addok-data
 else
-	rm -rf addok-data
+	rm -rf addok-data/*
 fi
 
 wget "http://bano.openstreetmap.fr/BAN_odbl/BAN_odbl_31-json.bz2" -P ./addok-data
